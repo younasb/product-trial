5373dd1cc81706f4569154eed04c72624a43ecb5
Younes BENZAKOUR <younesbenzakour@gmail.com>
2024-12-26 17:34:20 +0100

    refactor: remove unused  interface from core types

--- back/src/core/types.ts
+++ back/src/core/types.ts
@@ -14,21 +14,6 @@ export interface ErrorResponse {
 	errorCode?: string;
 }
 
-export interface UserData {
-	uid: string;
-	firstName: string;
-	lastName: string;
-	gender: 'female' | 'male';
-	date: string;
-	randomString1: string;
-	randomString2: string;
-	randomString3: string;
-	randomString4: string;
-	randomString5: string;
-	randomString6: string;
-	randomString7: string;
-}
-
 export interface HttpConfig {
 	headers?: Record<string, string>;
 	controller?: AbortController;
